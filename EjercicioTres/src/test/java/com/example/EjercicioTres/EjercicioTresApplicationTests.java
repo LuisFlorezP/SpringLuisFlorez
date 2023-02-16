2b6a91dc5eeeddf878a252955ca0fc18e3e7fc0f
Luis Florez <braian1k3@gmail.com>
2023-02-16 13:38:06 -0500

    Finalizar el ejercicio Tres del Bootcamp de Spring.

---
+++ EjercicioTres/src/test/java/com/example/EjercicioTres/EjercicioTresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EjercicioTres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioTresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
