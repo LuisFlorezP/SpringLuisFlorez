8989ad53018aae2d93b1e181d7981d062ddd6d8b
Luis Florez <braian1k3@gmail.com>
2023-02-20 21:48:44 -0500

    Crear nuevo proyecto de Spring para el ejercicio Dos del momento Dos.

---
+++ MomentoDos/EjercicioDos/src/main/java/com/example/EjercicioDos/EjercicioDosApplication.java
@@ -0,0 +1,13 @@
+package com.example.EjercicioDos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioDosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioDosApplication.class, args);
+	}
+
+}
---
+++ MomentoDos/EjercicioDos/src/test/java/com/example/EjercicioDos/EjercicioDosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EjercicioDos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioDosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
