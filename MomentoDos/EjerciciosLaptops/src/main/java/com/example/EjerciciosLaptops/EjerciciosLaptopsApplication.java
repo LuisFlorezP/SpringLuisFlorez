823b5a70f6e46d595a6c655c4d16054d90e4ef67
Luis Florez <braian1k3@gmail.com>
2023-02-20 23:07:03 -0500

    Desarrollo del ejericio Dos del momento DOs del Bootcamp de Spring.

--- MomentoDos/EjerciciosLaptops/src/main/java/com/example/EjerciciosLaptops/EjerciciosLaptopsApplication.java
+++ MomentoDos/EjerciciosLaptops/src/main/java/com/example/EjerciciosLaptops/EjerciciosLaptopsApplication.java
@@ -2,12 +2,13 @@ package com.example.EjerciciosLaptops;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class EjerciciosLaptopsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EjerciciosLaptopsApplication.class, args);
+		ApplicationContext context = SpringApplication.run(EjerciciosLaptopsApplication.class, args);
 	}
 
 }
